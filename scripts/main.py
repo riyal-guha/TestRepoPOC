8eba6add1380d42500421b82829726a5ae783746
Riyal Guha <pnl1bj16@emea.royalahold.net>
2025-05-30 10:49:03 +0000

    hardoce_json

--- scripts/main.py
+++ scripts/main.py
@@ -1,1 +1,16 @@
-print("Testing python run")
+import json
+data = {
+    "messageId": "12345",
+    "eventType": "CreateActionPlan",
+    "timestamp": "2025-05-20T12:34:56Z",
+    "data": {
+        "flowId": "flowId123",
+        "userId": "pnl0usXX",
+        "nlp": "Login to SNOW and view all tickets in all status",
+        "actionPlan": """Open https://aholddelhaize.service-now.com/
+Login with user pnl0us72 and password xxxxxxxxx
+View all tickets in all status
+logout"""
+    }
+}
+print(json.dumps(data, indent=2))
